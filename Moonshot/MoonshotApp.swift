37aee1385f682d7989b2610398c8b190ff28e6f2
nomad483 <nicolaizakluka@gmail.com>
2023-04-30 20:18:13 +0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Mykola Zakluka on 30.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
